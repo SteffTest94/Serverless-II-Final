1e9d489ac511a9601cd89666a2a077c1d7bb29be
Your Name <you@example.com>
2024-05-11 17:43:28 -0500

    finally figured out a fix. its ridiculous how our instructor keeps giving us projects based on outdated code that can't be used with our tools, without giving us the info on how to resolve it or how to improve it

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -2,8 +2,7 @@ export type AmplifyDependentResourcesAttributes = {
   "api": {
     "serverlessiifinal": {
       "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
+      "GraphQLAPIIdOutput": "string"
     }
   },
   "auth": {
